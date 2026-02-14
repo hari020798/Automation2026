44e99b3ae8ea6a21f3e67c32a95aa8481108b7fb
Hariharan S <hariharan.shanmugam@photon.com>
2026-02-14 11:03:57 +0530

    Added data provider class

--- src/test/java/com/Automation2026/DP.java
+++ src/test/java/com/Automation2026/DP.java
@@ -1,5 +1,18 @@
 package com.Automation2026;
 
+import org.testng.annotations.Test;
+
 public class DP {
+	
+	
+	
+//	data
+	@Test( dataProviderClass =  DataProvider.class)
+	private void Bio(String str1, String str2) {
+		
+		System.out.println(str1 + str2);
+
+	}
+    
 
 }
--- src/test/java/com/Automation2026/DataProvider.java
+++ src/test/java/com/Automation2026/DataProvider.java
@@ -1,8 +1,26 @@
 package com.Automation2026;
 
+
 public class DataProvider {
 	
+    
+	@org.testng.annotations.DataProvider(indices = {1,2}, parallel =  !true)
 	
-	
+	public static String[][] StringMethods() {
+		
+		String [] [] data = new String [3] [2];
+		
+		data [0] [0] = "Hariharan";
+		data [0] [1] = "25";
+		
+		data [1] [0] = "Hanni";
+		data [1] [1] = "30";
+		
+		data [2] [0] = "Resh";
+		data [2] [1] = "24";
+		
+		return data;
+		
+	}
 
 }
